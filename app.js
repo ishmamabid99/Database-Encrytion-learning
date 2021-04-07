81df8c7c909710c8ae7cb4de2b6cfa9014537ede
ishmamabid99 <ishmam.abid5422@gmail.com>
2021-04-07 20:58:14 +0600

    bcrypt tutorial

--- app.js
+++ app.js
@@ -4,58 +4,67 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+//const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 const app = express();
 app.use(express.static("public"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{
-  useNewUrlParser:true,
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
-const userSchema =new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-console.log(secret);
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
+//console.log(secret);
+//userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 
-const User = new mongoose.model("user",userSchema);
+const User = new mongoose.model("user", userSchema);
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
   res.render("login");
 });
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register",(req,res)=>{
-  const newUser = new User({
-    email: req.body.username,
-    password:req.body.password
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    console.log(hash);
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save((err) => {
+      if (!err) res.render("secrets");
+    });
   });
-  newUser.save((err)=>{
-    if(!err) res.render("secrets");
-  })
 });
-app.post("/login",(req,res)=>{
-  User.findOne({email:req.body.username},(err , foundItem)=>{
-    if(err) console.log(err);
-    else{
-      if(foundItem && foundItem.password === req.body.password){
-        res.render("secrets");
-      }
+app.post("/login", (req, res) => {
+  User.findOne({
+    email: req.body.username
+  }, (err, foundItem) => {
+    if (err) console.log(err);
+    else {
+      bcrypt.compare(req.body.password , foundItem.password , (err ,result)=>{
+        if(result) res.render("secrets");
+      })
     }
-  })
+  });
 });
-app.listen(3000 ,()=>{
+app.listen(3000, () => {
   console.log("listening to port 3000");
 })
