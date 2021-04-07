349be2e72705fe0fd3985181d9d430b30a4475e9
ishmamabid99 <ishmam.abid5422@gmail.com>
2021-04-07 13:24:09 +0600

    1st level of Auth

---
+++ app.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{
+  useNewUrlParser:true,
+  useUnifiedTopology: true
+});
+const userSchema =new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+console.log(secret);
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
+
+const User = new mongoose.model("user",userSchema);
+
+app.get("/",(req,res)=>{
+  res.render("home");
+});
+app.get("/login",(req,res)=>{
+  res.render("login");
+});
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+  const newUser = new User({
+    email: req.body.username,
+    password:req.body.password
+  });
+  newUser.save((err)=>{
+    if(!err) res.render("secrets");
+  })
+});
+app.post("/login",(req,res)=>{
+  User.findOne({email:req.body.username},(err , foundItem)=>{
+    if(err) console.log(err);
+    else{
+      if(foundItem && foundItem.password === req.body.password){
+        res.render("secrets");
+      }
+    }
+  })
+});
+app.listen(3000 ,()=>{
+  console.log("listening to port 3000");
+})
